dd790e1524a79ba3fbe219567fc08b027eb2afc5
Ndiaga GUEYE <ndiagaa.gueye@gmail.com>
2021-12-22 21:08:27 +0000

    Backend: service group

--- chat-backend/app/routes/message/message.route.js
+++ chat-backend/app/routes/message/message.route.js
@@ -7,9 +7,7 @@ messageRouter.get("/chat/users", messages.getAllUsers);
 messageRouter.get("/chat/users-discussions", messages.getUsersDiscussions);
 messageRouter.get("/chat/groupes-discussions", messages.getGroupeDiscussions);
 
-
-/*
-messageRouter.get("/chat/message/:id", messages.findOne);
+/*messageRouter.get("/chat/message/:id", messages.findOne);
 messageRouter.put("/chat/message/:id", messages.update);
 messageRouter.delete("/chat/message/:id", messages.delete);
 messageRouter.delete("/chat/message/", messages.deleteAll);
